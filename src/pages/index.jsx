1eb840bda7e4e8a6118f1aeff4d885797eafce9a
brunosousa6 <50246786+brunosousa6@users.noreply.github.com>
2025-05-14 10:37:40 -0300

    teste3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import { useState } from "react";
 import { Mail, Phone } from "lucide-react";
 
